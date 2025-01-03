e7b2e4c005c8f05ec04fbbd3881fcc3e4c5f4514
uqhmcdou_local <uqhmcdou_local@SMP-F3NGMX3.uq.edu.au>
2025-01-03 16:13:52 +1000

    autocommit

---
+++ talks/_render.py
@@ -0,0 +1,14 @@
+from glob import glob
+import numpy as np
+
+for folder in glob("*/"):
+    out = open(folder + "_page.md",'w')
+    files = glob(folder+"*.jpg")
+    numbers = [int(filename.replace(folder+"Slide","").replace(".JPG","")) for filename in files]
+
+    files = np.array(files)
+    indices = np.argsort(numbers)
+    files = files[indices]
+    for file in files:
+        out.write("![image](%s) \n \\" %file.replace("folder", "./" ))
+    out.close()
