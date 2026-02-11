680125cdead4a8c62a632a207cacffe3df0fd472
uqhmcdou_local <uqhmcdou_local@SMP-F3NGMX3.uq.edu.au>
2026-02-11 10:13:10 +0800

    autocommit

--- portfolio/animation/cool_graphs/anim_gen/_make.py
+++ portfolio/animation/cool_graphs/anim_gen/_make.py
@@ -9,9 +9,13 @@ import subprocess
 import sys
 
 filenames = glob("*.bmp")
+print("Found %i files. Making..." %len(filenames))
 images = []
 
-for filename in filenames:
+for i, filename in enumerate(filenames):
+    if i % (len(filenames)//10)==0: print(i, end=', ')
     images.append(imageio.imread(filename))
-    
+
+print("Making")    
 imageio.mimsave('./_movie.gif', images, fps=48, loop=0)
+print("Done.")
